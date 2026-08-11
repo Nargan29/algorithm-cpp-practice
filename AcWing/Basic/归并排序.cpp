4c8f2a6ba681c5f062a3ca2a50daab1c28c2ac16
Nargan29 <2218361819@qq.com>
2026-08-11 17:08:29 +0800

    更新内容

--- AcWing/Basic/归并排序.cpp
+++ AcWing/Basic/归并排序.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
-const int N = 10010;
+const int N = 100010;
 int n;
 int q[N],temp[N];
 void merge_sort(int q[],int l,int r)
 {
-    if(l>=r) return;
-    int mid=(l+r)/2;
-    merge_sort(q,l,mid);
-    merge_sort(q,mid+1,r);
-    int k=0,i=l,j=mid+1;
-    while(i<=mid&&j<=r)
+   if(l>=r) return;
+   int mid=(l+r)/2;
+   merge_sort(q,l,mid);
+   merge_sort(q,mid+1,r);
+   int k=0,i=l,j=mid+1;
+   while(i<=mid&&j<=r)
         if(q[i]<=q[j]) temp[k++]=q[i++];
         else temp[k++]=q[j++];
     while(i<=mid) temp[k++]=q[i++];
     while(j<=r) temp[k++]=q[j++];
 
-    for(int i=l,j=0;i<=r,j<k;i++,j++) q[i]=temp[j];
+    for(i=l,j=0;i<=r,j<k;i++,j++) q[i]=temp[j];
 }
 int main()
 {
