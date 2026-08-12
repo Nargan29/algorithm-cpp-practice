0f3690a8afb26c5fe6d2a994ff39cc0534281791
Nargan29 <2218361819@qq.com>
2026-08-12 21:51:44 +0800

    更新

--- AcWing/Basic/三次方跟.cpp
+++ AcWing/Basic/三次方跟.cpp
@@ -5,13 +5,13 @@ int main()
 {
     double x;
     cin>>x;
-    int l=-10000,r=10000;
+    double l=-10000,r=10000;
     while(r-l>1e-8)
     {
         double mid=(l+r)/2;
         if(mid*mid*mid>=x) r=mid;
         else l=mid;
     }
-    cout<<fixed<<setprecision(2)<<l<<endl;
+    cout<<fixed<<setprecision(6)<<l<<endl;
     return 0;
 }
